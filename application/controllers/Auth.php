042adefd594696db32aa98e4eb0f67c1597a46a4
CHOI JINYOUNG <chio663200@gmail.com>
2025-10-28 10:45:08 +0900

    feat : 회원가입시 이름 필수

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -27,20 +27,22 @@ class Auth extends MY_Controller
     }
 
     public function register_process() {
+        $this->form_validation->set_rules('username', '이름 (닉네임)', 'required|trim');
         $this->form_validation->set_rules('user_id', '아이디', 'required|is_unique[users.user_id]');
         $this->form_validation->set_rules('user_password', '비밀번호', 'required');
 
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('register_view');
         } else {
+            $username = $this->input->post('username');
             $user_id = $this->input->post('user_id');
             $password = $this->input->post('user_password');
 
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-            $this->User_model->create_user($user_id, $hashed_password);
+            $this->User_model->create_user($username, $user_id, $hashed_password);
 
-            redirect('/login');
+            redirect('auth/login');
         }
     }
 
