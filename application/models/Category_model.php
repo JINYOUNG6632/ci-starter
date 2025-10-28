49e313c22527bd913aad4183d826156453b1978a
CHOI JINYOUNG <chio663200@gmail.com>
2025-10-28 14:55:10 +0900

    feat: 카테고리 모델 작성

---
+++ application/models/Category_model.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Category_model extends MY_Model {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function get_all_categoreis()
+    {
+        $query = $this->db->get('categories');
+        return $query->result_array();
+    }
+
+    public function get_category_by_id($id)
+    {
+        $query = $this->db->get_where('categories', array('id' => $id));
+        return $query->row();
+    }
+}
