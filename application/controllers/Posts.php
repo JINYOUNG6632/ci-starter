73843da43a7cf71b9e5b9e05bf91034c67f756c0
CHOI JINYOUNG <chio663200@gmail.com>
2025-10-29 14:04:00 +0900

    feat : view로드하던것 template로 변경

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -9,9 +9,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @property Post_model $Post_model
  * @property Category_model $Category_model
  */
-class Posts extends Member_Controller
+class Posts extends MY_Controller
 {
-    
     public $table = 'posts';
 
     public function __construct()
@@ -21,103 +20,191 @@ class Posts extends Member_Controller
         $this->load->model('Category_model');
     }
 
-    public function index($category_id = 1) 
+    private function _check_login()
     {
-        $data['posts'] = $this->Post_model->get_posts_by_category($category_id);
-        $data['category'] = $this->Category_model->get_category_by_id($category_id);
+        if (!$this->session->userdata('logged_in')) {
+            $this->session->set_flashdata('error', '로그인이 필요합니다.');
+            redirect('auth/login');
+        }
+    }
 
-        $this->load->view('list_view', $data);
+    public function index($category_id = 1)
+    {
+        $data['posts']    = $this->Post_model->get_posts_by_category($category_id);
+        $data['category'] = $this->Category_model->get_category_by_id($category_id);
+        $data['title']    = $data['category']->name;
+
+        $this->template_->viewAssign($data);
+        $this->template_->viewDefine('content', 'list_view.tpl');
+        $this->template_->viewDefine('layout_common', 'true');
     }
 
     public function view($post_id)
     {
-        $data['post'] = $this->Post_model->get_post_by_id($post_id);
-        $this->load->view('post_detail_view', $data);
+        $data['post']            = $this->Post_model->get_post_by_id($post_id);
+        $data['error']           = $this->session->flashdata('error');
+        $data['session_user_id'] = $this->session->userdata('id');
+        $data['title']           = $data['post'] ? $data['post']->title : '게시글 상세';
+
+        $this->template_->viewAssign($data);
+        $this->template_->viewDefine('content', 'post_detail_view.tpl');
+        $this->template_->viewDefine('layout_common', 'true');
     }
 
     public function write_form()
     {
-        // 추후에 카테고리를 셀렉할 수 있도록 변경
-        // 추후 파일도 업로드 가능하도록 변경
-        $data['categories'] = $this->Category_model->get_all_categories();
-        $data['post'] = null;
+        $this->_check_login();
 
-        $this->load->view('post_form_view', $data);
+        $categories = $this->Category_model->get_all_categories();
+
+        $data = [
+            'is_edit'               => false,
+            'form_action'           => '/ci-starter/posts/write_process',
+            'validation_errors'     => validation_errors(),
+
+            'categories'            => $categories,
+            'selected_category_id'  => set_value('category_id', ''),
+
+            'title_value'           => set_value('title', ''),
+            'body_value'            => set_value('body', ''),
+
+            'title'                 => '새 게시글 작성'
+        ];
+
+        $this->template_->viewAssign($data);
+        $this->template_->viewDefine('content', 'post_form_view.tpl');
+        $this->template_->viewDefine('layout_common', 'true');
     }
 
     public function write_process()
     {
-        // 추후에 writeform에서 카테고리를 셀렉할 수 있도록 로딩할 때 변경
+        $this->_check_login();
+
         $this->form_validation->set_rules('category_id', '카테고리', 'required|integer');
         $this->form_validation->set_rules('title', '제목', 'required|trim');
         $this->form_validation->set_rules('body', '내용', 'required');
 
         if ($this->form_validation->run() == false) {
-            // 바로 입력창으로 다시 보내면 내용이 날아갈 수 있으니 방법 생각
-            $this->write_form();
-        } else {
-            $data = array(
-                'title' => $this->input->post('title'),
-                'body' => $this->input->post('body'),
-                'category_id' => $this->input->post('category_id'),
-                'user_id' => $this->session->userdata('id')
-            );
 
-            $new_post_id = $this->Post_model->create_post($data);
-            redirect('posts/view/' . $new_post_id);
+            $categories = $this->Category_model->get_all_categories();
+
+            $data = [
+                'is_edit'               => false,
+                'form_action'           => '/ci-starter/posts/write_process',
+                'validation_errors'     => validation_errors(),
+
+                'categories'            => $categories,
+                'selected_category_id'  => set_value('category_id', ''),
+
+                'title_value'           => set_value('title', ''),
+                'body_value'            => set_value('body', ''),
+
+                'title'                 => '새 게시글 작성',
+            ];
+
+            $this->template_->viewAssign($data);
+            $this->template_->viewDefine('content', 'post_form_view.tpl');
+            $this->template_->viewDefine('layout_common', 'true');
+            return;
         }
+
+        $insert = [
+            'title'       => $this->input->post('title'),
+            'body'        => $this->input->post('body'),
+            'category_id' => $this->input->post('category_id'),
+            'user_id'     => $this->session->userdata('id'),
+        ];
+
+        $new_post_id = $this->Post_model->create_post($insert);
+        redirect('posts/view/' . $new_post_id);
     }
 
     public function edit_form($post_id)
     {
-        $post = $this->Post_model->get_post_by_id($post_id);
+        $this->_check_login();
 
+        $post = $this->Post_model->get_post_by_id($post_id);
         if (!$post || $post->user_id != $this->session->userdata('id')) {
             $this->session->set_flashdata('error', '수정권한이 없습니다.');
             redirect('posts/view/' . $post_id);
             return;
         }
 
-        // 카테고리를 다 불러서 데이터에 넣어준다? 이상함
-        $data['categories'] = $this->Category_model->get_all_categories();
-        $data['post'] = $post;
+        $categories = $this->Category_model->get_all_categories();
 
-        $this->load->view('post_form_view', $data);
+        $data = [
+            'is_edit'               => true,
+            'form_action'           => '/ci-starter/posts/edit_process/' . $post->id,
+            'validation_errors'     => validation_errors(),
+
+            'categories'            => $categories,
+            'selected_category_id'  => set_value('category_id', $post->category_id),
+
+            'title_value'           => set_value('title', $post->title),
+            'body_value'            => set_value('body', $post->body),
+
+            'title'                 => '게시글 수정',
+        ];
+
+        $this->template_->viewAssign($data);
+        $this->template_->viewDefine('content', 'post_form_view.tpl');
+        $this->template_->viewDefine('layout_common', 'true');
     }
 
     public function edit_process($post_id)
     {
-        $post = $this->Post_model->get_post_by_id($post_id);
+        $this->_check_login();
 
+        $post = $this->Post_model->get_post_by_id($post_id);
         if (!$post || $post->user_id != $this->session->userdata('id')) {
             $this->session->set_flashdata('error', '권한이 없습니다.');
             redirect('posts/view/' . $post_id);
             return;
         }
 
-        // 마찬가지로 추후 파일추가시 변경
         $this->form_validation->set_rules('category_id', '카테고리', 'required|integer');
         $this->form_validation->set_rules('title', '제목', 'required|trim');
         $this->form_validation->set_rules('body', '내용', 'required');
 
-        if($this->form_validation->run() == false) {
-            $this->edit_form($post_id);
-        } else {
-            $data = array(
-                'title' => $this->input->post('title'),
-                'body' => $this->input->post('body'),
-                'category_id' => $this->input->post('category_id')
-            );
+        if ($this->form_validation->run() == false) {
 
-            $this->Post_model->update_post($post_id, $data);
-            redirect('posts/view/' . $post_id);
+            $categories = $this->Category_model->get_all_categories();
+
+            $data = [
+                'is_edit'               => true,
+                'form_action'           => '/ci-starter/posts/edit_process/' . $post->id,
+                'validation_errors'     => validation_errors(),
+
+                'categories'            => $categories,
+                'selected_category_id'  => set_value('category_id', $post->category_id),
+
+                'title_value'           => set_value('title', $post->title),
+                'body_value'            => set_value('body', $post->body),
+
+                'title'                 => '게시글 수정',
+            ];
+
+            $this->template_->viewAssign($data);
+            $this->template_->viewDefine('content', 'post_form_view.tpl');
+            $this->template_->viewDefine('layout_common', 'true');
+            return;
         }
+
+        $update = [
+            'title'       => $this->input->post('title'),
+            'body'        => $this->input->post('body'),
+            'category_id' => $this->input->post('category_id'),
+        ];
+
+        $this->Post_model->update_post($post_id, $update);
+        redirect('posts/view/' . $post_id);
     }
 
     public function delete($post_id)
     {
-        $post = $this->Post_model->get_post_by_id($post_id);
+        $this->_check_login();
 
+        $post = $this->Post_model->get_post_by_id($post_id);
         if (!$post || $post->user_id != $this->session->userdata('id')) {
             $this->session->set_flashdata('error', '권한이 없습니다.');
             redirect('posts/view/' . $post_id);
