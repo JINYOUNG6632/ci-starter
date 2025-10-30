fc74ec9e3352a4f8149290f32e84d926e728bbe7
CHOI JINYOUNG <chio663200@gmail.com>
2025-10-30 13:38:53 +0900

    refactor : css 및 js로딩 optimizer로 위임

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -33,14 +33,23 @@ class Posts extends MY_Controller
         $posts    = $this->Post_model->get_posts_by_category($category_id);
         $category = $this->Category_model->get_category_by_id($category_id);
 
+        // 공통 CSS는 BASE_CSS에 직접 링크
+        $baseCss = '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">';
+
+        // 화면 전용 CSS/JS는 Optimizer 사용
+        $this->css('list_view.css', '20251030'); // 캐시 무력화 버전
+        // $this->js('list_view.js', '20251030');  // 필요 시
+
+        $tags = $this->optimizer->makeOptimizerScriptTag();
+
         $data = [
             'posts'    => $posts,
             'category' => $category,
-            'title'    => $category->name,
+            'title'    => $category ? $category->name : '게시글 목록',
 
-            'BASE_CSS' => '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">',
-            'CSS'      => '<link rel="stylesheet" href="/ci-starter/assets/css/list_view.css">',
-            'JS'       => '',
+            'BASE_CSS' => $baseCss,                // 공통
+            'CSS'      => $tags['css_optimizer'],  // 화면 전용
+            'JS'       => $tags['js_optimizer'],   // 화면 전용
         ];
 
         $this->template_->viewAssign($data);
@@ -52,14 +61,29 @@ class Posts extends MY_Controller
     {
         $post = $this->Post_model->get_post_by_id($post_id);
 
+        // 공통 CSS
+        $baseCss = '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">';
+
+        // 화면 전용
+        $this->css('post_detail.css', '20251030');
+        $this->js('post_detail.js', '20251030');
+
+        $tags = $this->optimizer->makeOptimizerScriptTag();
+
         $data = [
             'post'             => $post,
+            'post_id'          => $post ? $post->id : null,
             'session_user_id'  => $this->session->userdata('id'),
             'title'            => $post ? $post->title : '게시글 상세',
-            'BASE_CSS'         => '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">',
-            'CSS'              => '<link rel="stylesheet" href="/ci-starter/assets/css/post_detail.css">',
-            'JS'               => '<script src="/ci-starter/assets/js/post_detail.js"></script>',
+
+            'BASE_CSS'         => $baseCss,               // 공통
+            'CSS'              => $tags['css_optimizer'], // 화면 전용
+            'JS'               => $tags['js_optimizer'],  // 화면 전용
         ];
+
+        $this->template_->viewDefine('comment_form', 'comment_form.tpl');
+        $this->template_->viewDefine('comment_list_stub', 'comment_list_stub.tpl');
+        $this->template_->viewDefine('comment_section', 'comment_section.tpl');
 
         $this->template_->viewAssign($data);
         $this->template_->viewDefine('content', 'post_detail_view.tpl');
@@ -72,6 +96,14 @@ class Posts extends MY_Controller
 
         $categories = $this->Category_model->get_all_categories();
 
+        // 공통 CSS
+        $baseCss = '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">';
+
+        // 화면 전용
+        $this->css('post_form_view.css', '20251030');
+
+        $tags = $this->optimizer->makeOptimizerScriptTag();
+
         $data = [
             'is_edit'               => false,
             'form_action'           => '/ci-starter/posts/write_process',
@@ -83,9 +115,9 @@ class Posts extends MY_Controller
             'body_value'            => set_value('body', ''),
             'title'                 => '새 게시글 작성',
 
-            'BASE_CSS' => '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">',
-            'CSS'      => '<link rel="stylesheet" href="/ci-starter/assets/css/post_form_view.css">',
-            'JS'       => '',
+            'BASE_CSS' => $baseCss,
+            'CSS'      => $tags['css_optimizer'],
+            'JS'       => $tags['js_optimizer'],
         ];
 
         $this->template_->viewAssign($data);
@@ -105,6 +137,13 @@ class Posts extends MY_Controller
 
             $categories = $this->Category_model->get_all_categories();
 
+            // 공통 CSS
+            $baseCss = '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">';
+
+            // 화면 전용
+            $this->css('post_form_view.css', '20251030');
+            $tags = $this->optimizer->makeOptimizerScriptTag();
+
             $data = [
                 'is_edit'               => false,
                 'form_action'           => '/ci-starter/posts/write_process',
@@ -117,6 +156,10 @@ class Posts extends MY_Controller
                 'body_value'            => set_value('body', ''),
 
                 'title'                 => '새 게시글 작성',
+
+                'BASE_CSS' => $baseCss,
+                'CSS'      => $tags['css_optimizer'],
+                'JS'       => $tags['js_optimizer'],
             ];
 
             $this->template_->viewAssign($data);
@@ -149,6 +192,13 @@ class Posts extends MY_Controller
 
         $categories = $this->Category_model->get_all_categories();
 
+        // 공통 CSS
+        $baseCss = '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">';
+
+        // 화면 전용
+        $this->css('post_form_view.css', '20251030');
+        $tags = $this->optimizer->makeOptimizerScriptTag();
+
         $data = [
             'is_edit'               => true,
             'form_action'           => '/ci-starter/posts/edit_process/' . $post->id,
@@ -161,6 +211,10 @@ class Posts extends MY_Controller
             'body_value'            => set_value('body', $post->body),
 
             'title'                 => '게시글 수정',
+
+            'BASE_CSS' => $baseCss,
+            'CSS'      => $tags['css_optimizer'],
+            'JS'       => $tags['js_optimizer'],
         ];
 
         $this->template_->viewAssign($data);
@@ -187,6 +241,13 @@ class Posts extends MY_Controller
 
             $categories = $this->Category_model->get_all_categories();
 
+            // 공통 CSS
+            $baseCss = '<link rel="stylesheet" href="/ci-starter/assets/css/layout_common.css">';
+
+            // 화면 전용
+            $this->css('post_form_view.css', '20251030');
+            $tags = $this->optimizer->makeOptimizerScriptTag();
+
             $data = [
                 'is_edit'               => true,
                 'form_action'           => '/ci-starter/posts/edit_process/' . $post->id,
@@ -199,6 +260,10 @@ class Posts extends MY_Controller
                 'body_value'            => set_value('body', $post->body),
 
                 'title'                 => '게시글 수정',
+
+                'BASE_CSS' => $baseCss,
+                'CSS'      => $tags['css_optimizer'],
+                'JS'       => $tags['js_optimizer'],
             ];
 
             $this->template_->viewAssign($data);
